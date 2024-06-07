9c4b696e0fc734f29fc7a50f119ba7b52a8003be
joaquinftRIVERPLATE <joaquisft@gmail.com>
2024-06-07 09:22:31 -0300

    Initial commit

---
+++ Assets/ScoreCounter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreCounter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text txtScore;
+    public int Score;
+    void Start()
+    {
+        txtScore.text = "0";
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Contacto");
+        AlimentoScript alimento;
+        alimento = collision.gameObject.GetComponent<AlimentoScript>();
+        Score += alimento.valorAlimentario;
+        txtScore.text = Score.ToString();
+    }
+}
